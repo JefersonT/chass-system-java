e0f6a33d14ab59ff65b423c74489c5a97208de4b
JefersonT <tqi_jtorres@uolinc.com>
2023-10-11 21:48:45 -0300

    first commit

---
+++ src/test/java/br/com/chesssystemjava/ChessSystemJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chesssystemjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSystemJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
