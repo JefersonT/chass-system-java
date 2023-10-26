da231fafd5b646dd4697ed5794e7de1231e860a8
JefersonT <tqi_jtorres@uolinc.com>
2023-10-25 21:11:20 -0300

    Implementing possible moves of Rock

--- src/main/java/br/com/chesssystemjava/chess/ChessPiece.java
+++ src/main/java/br/com/chesssystemjava/chess/ChessPiece.java
@@ -2,17 +2,22 @@ package br.com.chesssystemjava.chess;
 
 import br.com.chesssystemjava.boardgame.Board;
 import br.com.chesssystemjava.boardgame.Piece;
+import br.com.chesssystemjava.boardgame.Position;
 import lombok.Getter;
-import lombok.Setter;
 
 public abstract class ChessPiece extends Piece {
 
-    @Getter @Setter
+    @Getter
     private Color color;
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p.getColor() != color;
     }
 
 }
