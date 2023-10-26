8bf115b0858bf8580c05a637c45fec9dce4e8ada
JefersonT <tqi_jtorres@uolinc.com>
2023-10-25 21:39:26 -0300

    Printing possible moves

--- src/main/java/br/com/chesssystemjava/ChessSystemJavaApplication.java
+++ src/main/java/br/com/chesssystemjava/ChessSystemJavaApplication.java
@@ -25,9 +25,14 @@ public class ChessSystemJavaApplication {
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
 				System.out.println();
 				System.out.println("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
+
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			} catch (ChessException e) {
